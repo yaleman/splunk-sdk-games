0e90b9f346b5a144122a478eb7fa9aabc252cb29
James Hodgkinson <james@terminaloutcomes.com>
2022-05-10 12:28:02 +1000

    cleaning up example writer

--- example_write_raw_json.py
+++ example_write_raw_json.py
@@ -19,37 +19,51 @@ import config
 TEST_SEARCH_TIME_LATEST = round(time(),3)-30 # 30 seconds ago
 TEST_SEARCH_TIME_EARLIEST = TEST_SEARCH_TIME_LATEST - 300 # five minutes before that
 
-test_search = (
-    f"""search index=_internal TERM(INFO) earliest={TEST_SEARCH_TIME_EARLIEST} latest={TEST_SEARCH_TIME_LATEST}
-    | table *
-    """
-)
+def main() -> None:
+    """ main function """
 
-# here we start the connection to the splunk server, ensuring that our login works
-service = client.connect(
-            host=config.host,
-            username=config.username,
-            password=config.password,
-            autoLogin=True,
-        )
+    test_search = (
+        f"""search index=_internal TERM(INFO) earliest={TEST_SEARCH_TIME_EARLIEST} latest={TEST_SEARCH_TIME_LATEST}
+        | table *
+        """
+    )
 
-job_config = {
-    # more fields more better
-    "adhoc_search_level": "verbose",
-    # unlimited results!
-    "count": 0,
-}
+    # here we start the connection to the splunk server, ensuring that our login works
+    service = client.connect(
+                host=config.host,
+                username=config.username,
+                password=config.password,
+                autoLogin=True,
+            )
 
-# this is where we start the job
-job = service.jobs.oneshot(test_search, **job_config)
+    job_config = {
+        # more fields more better
+        "adhoc_search_level": "verbose",
+        # unlimited results!
+        "count": 0,
+        # because JSON is way easier to parse than XML.
+        "output_mode" : "json",
+    }
 
-# start reading the first block from the response
-data = job.read(10240)
+    # this is where we start the job
+    print("Starting the search...")
+    job = service.jobs.oneshot(test_search, **job_config)
 
-with open("outputfile.json", "wb") as file_handle:
-    # write to disk in a loop
-    while data:
-        file_handle.write(data)
-        data = job.read(10240)
+    filename = "outputfile.json"
 
-# done!
+    print(f"Writing raw data to {filename}.", end="")
+    # start reading the first block from the response
+    data = job.read(10240)
+
+    with open(filename, "wb") as file_handle:
+        # write to disk in a loop
+        while data:
+            file_handle.write(data)
+            data = job.read(102400)
+            # progress indicator, without a newline
+            print(".", end="")
+
+    print("\nDone!") # a newline, for neatness.
+
+if __name__ == "__main__":
+    main()
