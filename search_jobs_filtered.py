f1d8efb442f17c4acb23e7130f880d604a683081
James Hodgkinson <james@terminaloutcomes.com>
2023-12-16 14:48:28 +1000

    adding search job tests

---
+++ search_jobs_filtered.py
@@ -0,0 +1,21 @@
+# search_jobs_filtered.py
+#
+# filter the user's jobs
+
+from splunklib import client
+
+import config
+
+service = client.connect(
+    host=config.host,
+    username=config.username,
+    password=config.password,
+    autologin=True,
+    app="yaleman_reports",
+    owner=config.username,
+)
+
+print(f"with isSavedSearch=1: {len(service.jobs.list(search='isSavedSearch=1'))}")
+print(f"with eventCount>1000: {len(service.jobs.list(search='eventCount>1000'))}")
+
+print(f"with empty search: {len(service.jobs.list())}")
